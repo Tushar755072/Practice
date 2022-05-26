05f65d80c74ddd938c3693c5f2c310e6efd10a18
Tushar55 <tusharnanaware55@gmail.com>
2022-05-26 22:06:59 +0530

    updated class

--- Abc.java
+++ Abc.java
@@ -1,4 +1,7 @@
 
-public class Abc {
-
+public class Abc 
+{
+public static void main(String[] args) {
+	System.out.println("GitHub Suceesful");
+}
 }
--- NewClass.java
+++ NewClass.java
@@ -6,5 +6,6 @@ public static void main(String[] args)
 	System.out.println("hello");
 	System.out.println("Hello Again");
 	System.out.println("123445");
+	System.out.println("updated");
 }
 }
