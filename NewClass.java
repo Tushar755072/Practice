b59912b65396d5af6959becbf11a59322267f198
Tushar55 <tusharnanaware55@gmail.com>
2022-05-26 14:26:48 +0530

    added

--- NewClass.java
+++ NewClass.java
@@ -4,5 +4,7 @@ public class NewClass
 public static void main(String[] args) 
 {
 	System.out.println("hello");
+	System.out.println("Hello Again");
+	System.out.println("123445");
 }
 }
---
+++ Tushar.java
@@ -0,0 +1,8 @@
+
+public class Tushar
+{
+public static void main(String[] args) {
+	System.out.println("Welcome To GITHUB");
+	System.out.println("changes");
+}
+}
